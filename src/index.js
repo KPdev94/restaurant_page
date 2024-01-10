a058240dddcef85248a695bffe9435a30c674947
MunchingKitten <139931826+MunchingKitten@users.noreply.github.com>
2024-01-09 19:17:33 -0500

    General progress made to homepage

--- src/index.js
+++ src/index.js
@@ -4,23 +4,53 @@ import Tatas from './taco_tatas.jpg';
 
 let content = document.getElementById('content');
 
-function createBase() {
-    let headingDiv = document.createElement('div');
+function createHome() {
+    let homeDiv = document.createElement('div');
+    homeDiv.setAttribute('id', 'heading');
 
-    let title = document.createElement('h1');
-    title.innerHTML = "Tacos & Tatas!";
-    
     let tatasPic = document.createElement('img');
     tatasPic.src = Tatas;
     
 
     let blurb = document.createElement('p');
-    blurb.innerHTML = "Tacos, a staple of the hispanic community at large.\nAs well as Tatas, a staple of many amazing chefs.\nTogether they bring joy to multiple senses, especially a mouth-watering good meal!";
+    blurb.textContent = "Tacos, a staple of the hispanic community at large. As well as Tatas, a staple of many amazing chefs. Together they bring joy to multiple senses, especially a mouth-watering good meal!";
     
-    headingDiv.appendChild(title);
-    headingDiv.appendChild(tatasPic);
-    headingDiv.appendChild(blurb);
+    homeDiv.appendChild(tatasPic);
+    homeDiv.appendChild(blurb);
 
-    return headingDiv;
+    return homeDiv;
 }
-content.appendChild(createBase());
+
+function createTheme() {
+    let heading = document.createElement('heading');
+    heading.setAttribute('id', 'heading');
+
+    let title = document.createElement('div');
+    title.setAttribute('id', 'title');
+    title.textContent = 'TACOS AND TATAS';
+
+    let navBar = document.createElement('navBar');
+    navBar.setAttribute('id', 'navBar');
+
+    let homeButton = document.createElement('button');;
+    homeButton.setAttribute('id', 'home');
+    homeButton.textContent = 'HOME';
+
+    let menuButton = document.createElement('button');
+    menuButton.setAttribute('id', 'menu');
+    menuButton.textContent = 'MENU';
+
+    let contactButton = document.createElement('button')
+    contactButton.setAttribute('id', 'contact');
+    contactButton.textContent = 'CONTACT';
+    
+    content.appendChild(heading);
+    heading.appendChild(title);
+    heading.appendChild(navBar);
+    navBar.appendChild(homeButton);
+    navBar.appendChild(menuButton);
+    navBar.appendChild(contactButton);
+
+    return navBar;
+}
+content.appendChild(createTheme());
